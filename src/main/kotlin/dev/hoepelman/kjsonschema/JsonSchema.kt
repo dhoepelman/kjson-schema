53d16442f93f89981f9127a0cae180cd7200fabd
David Hoepelman <992153+dhoepelman@users.noreply.github.com>
2023-01-12 21:58:57 +0100

    Add builder

--- src/main/kotlin/dev/hoepelman/kjsonschema/JsonSchema.kt
+++ src/main/kotlin/dev/hoepelman/kjsonschema/JsonSchema.kt
@@ -1,16 +1,34 @@
 package dev.hoepelman.kjsonschema
 
-/** Representation of an object JSON Schema. */
-abstract class JsonSchema {
-    val `$schema`: String? = null
-    val `$id`: String? = null
-    val title: String? = null
 
 
-    data class `2020-12`(
-        override val `$id`: String?,
-        override val title: String?
-    ): JsonSchema {
-        override val `$schema`: String = "https://json-schema.org/draft/2020-12/schema"
+data class JsonSchema(
+    val `$schema`: String? = null,
+    val `$id`: String? = null,
+    val title: String? = null,
+) {
+    private constructor(builder: Builder): this(builder.`$schema`, builder.`$id`, builder.title)
+
+    companion object {
+        inline fun jsonSchema(dialect: Dialect = Dialect.V2020_12, block: Builder.() -> Unit) = Builder(dialect).apply(block).build()
+    }
+
+    enum class Dialect(val uri: String) {
+        DRAFT_07("http://json-schema.org/draft-07/schema#"),
+        V2019_09("https://json-schema.org/draft/2019-09/schema"),
+        V2020_12("https://json-schema.org/draft/2020-12/schema"),
+    }
+
+    class Builder(dialect: Dialect) {
+        var `$schema`: String? = null
+        var `$id`: String? = null
+        var title: String? = null
+        fun build() = JsonSchema(this)
+
+        init {
+            `$schema` = dialect.uri
+        }
     }
 }
+
+
